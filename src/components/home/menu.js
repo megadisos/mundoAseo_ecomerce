25c8010837b9672d7102f4f15874023de893945f
Jorge Reales <jorge.reales4@gmail.com>
2021-02-17 18:45:06 -0500

    enable menu

--- src/components/home/menu.js
+++ src/components/home/menu.js
@@ -7,9 +7,9 @@ function Menu(props){
       
         <nav className="navbar nav-menu navbar-expand-lg navbar-expand-md">
   <a className="navbar-brand" href="#"><img src={logo} className="img-fluid" width="200" height="80"></img></a>
-  {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
-  </button> */}
+  </button>
   <div className="navbar " >
     <div className="navbar-nav">
       <a className={location.pathname === "/" ? "nav-item it-menu nav-link active":"nav-item nav-link active"}  href="#">INICIO <span className="sr-only">(current)</span></a>
