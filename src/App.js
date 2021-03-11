0bbf394af7e00436302011f61145fccc20f76298
Jorge Reales <jorge.reales4@gmail.com>
2021-03-10 20:51:57 -0500

    cart_workingv7

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ fetch("https://www.nabtastore.com.co/api-ma/Productos/",{
                     method: 'GET',
                     headers: {
                       'Content-Type':'application/json',
-                      'Authorization': `Token 8056a54741f0eda31a7780ad71d24ef9667ce71c`
+                      'Authorization': `Token 427bd7635e8f0a0cf4c5b8317e9615044e344e92`
                     }
                   })
                   .then( resp => resp.json())
