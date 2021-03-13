1768e1cf0e17cfba1843a0b4b7c08cb86544807d
Jorge Reales <jorge.reales4@gmail.com>
2021-03-13 15:42:18 -0500

    comparacionv2

--- src/components/home/bseller.js
+++ src/components/home/bseller.js
@@ -16,37 +16,40 @@ function Bseller(props){
             </div>
             <center><div class="dropdown-divider in-div "></div></center>
             <div className="row">
-            {items && items.slice(0).reverse().filter(pr => pr.get_marc !== "mundo_aseo").map(item=>{
-                    return (
-                        <div key={item.id} className="col-lg-2 col-md-2 col-sm-12 product">
-                            <center><img src={item.get_path} onClick={()=> props.ProductLink(item.id,item.titulo)} class="img-fluid mt-2 img-prods" alt="Responsive image" />
-                            <p className="precio mt-1">${item.precio}</p>
-                            <p className="titulo ">{item.titulo}</p>
-                            <button type="button" onClick={e => props.CartAdd(item.id,item.precio) } data-toggle="modal"  data-target="#modal" class="btn btn-primary btn-cart">Agregar al carrito</button></center>
+            {items && items.slice(0).map(item=>{
+              if(item.get_marc !== "mundo_aseo"){
+                return (
+                  <div key={item.id} className="col-lg-2 col-md-2 col-sm-12 product">
+                      <center><img src={item.get_path} onClick={()=> props.ProductLink(item.id,item.titulo)} class="img-fluid mt-2 img-prods" alt="Responsive image" />
+                      <p className="precio mt-1">${item.precio}</p>
+                      <p className="titulo ">{item.titulo}</p>
+                      <button type="button" onClick={e => props.CartAdd(item.id,item.precio) } data-toggle="modal"  data-target="#modal" class="btn btn-primary btn-cart">Agregar al carrito</button></center>
 
-                            
-                            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><img src={logo} width="150" height="30"></img></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body titulos" >
-        El producto fue agregado a tu carrito. Que quieres hacer?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary btn-cart-cont"  data-dismiss="modal">Seguir comprando</button>
-        <button type="button" onClick={e=> window.location.href = "https://master.dphw95lv4xi23.amplifyapp.com/carrito" } class="btn btn-primary btn-cart-pop">Ir al carrito</button>
-      </div>
-    </div>
-  </div>
+                      
+                      <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header">
+  <h5 class="modal-title" id="exampleModalLabel"><img src={logo} width="150" height="30"></img></h5>
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+<div class="modal-body titulos" >
+  El producto fue agregado a tu carrito. Que quieres hacer?
+</div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-secondary btn-cart-cont"  data-dismiss="modal">Seguir comprando</button>
+  <button type="button" onClick={e=> window.location.href = "https://master.dphw95lv4xi23.amplifyapp.com/carrito" } class="btn btn-primary btn-cart-pop">Ir al carrito</button>
+</div>
+</div>
+</div>
 </div>
 
-                            </div>
-                    )
+                      </div>
+              )
+              }
+                   
                 
             })}
       
