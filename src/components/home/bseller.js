92ac17b00673d6981b77cf4d571328241bbd5c53
Jorge Reales <jorge.reales4@gmail.com>
2021-03-15 21:12:07 -0500

    check sizev1

--- src/components/home/bseller.js
+++ src/components/home/bseller.js
@@ -25,7 +25,7 @@ function Bseller(props){
                       <center><img src={item.get_path} onClick={()=> props.ProductLink(item.id,item.titulo)} class="img-fluid mt-2 img-prods" alt="Responsive image" />
                       <p className="precio mt-1">${item.precio}</p>
                       <p className="titulo ">{item.titulo}</p>
-                      {size < 17 ? <div><p></p> <p></p></div>: null}
+                     
                       <button type="button" style={size <= 17 ? "margin-top:10px;" : "margin-top:0px;"} onClick={e => props.CartAdd(item.id,item.precio) } data-toggle="modal"  data-target="#modal" class="btn btn-primary btn-cart">Agregar al carrito</button></center>
 
                       
